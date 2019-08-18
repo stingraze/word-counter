8722c0afa1969ae634e4617f3ddcff254cb3c877
Tsubasa Kato <tsubasa@superai.online>
2019-08-18 09:37:10 +0900

    Added Word Counter that outputs to CSV

--- word-counter.py
+++ word-counter.py
@@ -1,18 +1,15 @@
-#Script from:https://codereview.stackexchange.com/questions/103773/count-the-frequency-of-words-in-a-text-file
-#Modified by Tsubasa Kato on April 26th, 2019 for personal use.
+#Written by Tsubasa Kato April 28th, 2019
+#Updated on August 18th, 2019
+#Used and modified code from: https://stackoverflow.com/questions/16801246/how-do-you-write-a-counter-to-a-file-in-order
 from collections import Counter
-#change the file name to your needs.
-with open("./thoughts-cypher-stingraze.txt") as f:
-	wordcount = Counter(f.read().split())
 
-#To print sorted wordcount:
-#print (sorted(wordcount))
 
-path = "./wordcount.txt"
-#Open path and write to file
-with open(path, mode='w') as f:
-    f.write('\n'.join(wordcount))
+with open('./input.txt', 'r') as inputfile:
+    data = inputfile.read().split()
 
-#open file and  print each line
-with open(path) as f:
-    print(f.read())
+counted = Counter(data)
+
+#Outputs to simple CSV (not cleaned)
+with open("./word-freq.csv" , mode='w') as outputfile:
+    for k,v in  counted.most_common():
+        outputfile.write( "\"{}\" \"{}\"\n".format(k,v) )
